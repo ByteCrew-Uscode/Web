c58f30633ceb46e5adb57d94cb13564f09b1c437
cho4u4o <cho4u4o@gmail.com>
2025-06-28 15:29:51 +0900

    initial setting

---
+++ src/app/main.tsx
@@ -0,0 +1,13 @@
+import { createRoot } from "react-dom/client";
+import "./global.css";
+import "@stackflow/plugin-basic-ui/index.css";
+import App from "./App";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+
+const queryClient = new QueryClient();
+
+createRoot(document.getElementById("root")!).render(
+  <QueryClientProvider client={queryClient}>
+    <App />
+  </QueryClientProvider>
+);
