c19dabf4d87ecd4671315d27b95edc89813598a1
cho4u4o <cho4u4o@gmail.com>
2025-06-28 17:19:59 +0900

    feat : set root render point

--- src/app/main.tsx
+++ src/app/main.tsx
@@ -1,13 +1,13 @@
-import { createRoot } from "react-dom/client";
-import "./global.css";
-import "@stackflow/plugin-basic-ui/index.css";
-import App from "./App";
-import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { createRoot } from 'react-dom/client';
+import '@stackflow/plugin-basic-ui/index.css';
+import './global.css';
+import App from './App';
+import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 
 const queryClient = new QueryClient();
 
-createRoot(document.getElementById("root")!).render(
+createRoot(document.getElementById('root')!).render(
   <QueryClientProvider client={queryClient}>
     <App />
-  </QueryClientProvider>
+  </QueryClientProvider>,
 );
