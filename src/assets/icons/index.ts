f2b5d86636d50a6e743461d6fdcea60cdab8114e
cho4u4o <cho4u4o@gmail.com>
2025-06-28 17:20:20 +0900

    feat : add icon assets

---
+++ src/assets/icons/index.ts
@@ -0,0 +1,27 @@
+import Logo from './icon-tractor.png';
+import CameraIcon from './icon-camera.svg';
+import TractorBlackIcon from './icon-tractor-black.svg';
+import PhoneIcon from './icon-phone.svg';
+import CameraSolidIcon from './icon-camera-solid.svg';
+import EditIcon from './icon-edit.svg';
+import HomeIcon from './icon-home.svg';
+import UserIcon from './icon-user.svg';
+import ReservationIcon from './icon-reservation.svg';
+import HomeSelectedIcon from './icon-home-selected.svg';
+import UserSelectedIcon from './icon-user-selected.svg';
+import ReservationSelectedIcon from './icon-reservation-selected.svg';
+
+export {
+  Logo,
+  CameraIcon,
+  TractorBlackIcon,
+  PhoneIcon,
+  CameraSolidIcon,
+  EditIcon,
+  HomeIcon,
+  UserIcon,
+  ReservationIcon,
+  HomeSelectedIcon,
+  UserSelectedIcon,
+  ReservationSelectedIcon,
+};
